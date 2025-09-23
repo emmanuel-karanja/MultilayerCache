9581e2b1de8d5375c7284340efa7d1b1dcf89358
emmanuel-karanja <emmanuel.nganga.ke@gmail.com>
2025-09-23 12:50:14 +0300

    update demo

--- MultilayerCache/obj/Release/net7.0/MultilayerCache.AssemblyInfo.cs
+++ MultilayerCache/obj/Release/net7.0/MultilayerCache.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultilayerCache")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c72eb978b332f6603e8f0048d1a8e5c0fd5e92c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+858bc1e877e8ca68a4f3d3212db21292e8a7352d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultilayerCache")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultilayerCache")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
